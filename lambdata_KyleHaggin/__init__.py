2569d216ee935208081ad3f8f84164c9af031d85
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-01 13:03:03 -0700

    Add some proof of concept utility functions

---
+++ lambdata_KyleHaggin/__init__.py
@@ -0,0 +1,10 @@
+"""
+A Colleciton of data science helper functions
+"""
+
+import numpy as np
+import pandas as pd
+
+#sample and test code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
