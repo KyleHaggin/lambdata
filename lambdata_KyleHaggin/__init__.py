369fffea9498f496094aad77e1e47f40539e5b97
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-10-03 10:29:11 -0700

    Add sqrt, sqrt test, datafunctions. Updated code to conform to pep8 standard.

--- lambdata_KyleHaggin/__init__.py
+++ lambdata_KyleHaggin/__init__.py
@@ -5,6 +5,6 @@ A Colleciton of data science helper functions
 import numpy as np
 import pandas as pd
 
-#sample and test code
+# sample and test code
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
